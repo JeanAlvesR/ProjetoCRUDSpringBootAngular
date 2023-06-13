34f07097eb3eb74d823cbee0dd9d2f3cd0acb3fb
Jean Alves <engjeanr@gmail.com>
2023-06-13 11:37:38 -0300

    add Inteface AlunoService para injecao de dependencia via spring boot

--- src/main/java/github/jeanalvesr/domain/service/AlunoService.java
+++ src/main/java/github/jeanalvesr/domain/service/AlunoService.java
@@ -1,16 +1,16 @@
 package github.jeanalvesr.domain.service;
 
 
-import github.jeanalvesr.domain.entities.Aluno;
 import github.jeanalvesr.dto.AlunoDto;
+import github.jeanalvesr.dto.AlunoSaveDto;
 
 import java.util.List;
 
 public interface AlunoService {
     public List<AlunoDto> getAlunos();
-    public void criarAluno(AlunoDto alunoDto);
+    public Integer criarAluno(AlunoSaveDto alunoSaveDto);
 
-    public void removeAluno(Integer id);
+    public void removerAluno(Integer id);
 
     public void alterarAluno(AlunoDto alunoDTO);
 
