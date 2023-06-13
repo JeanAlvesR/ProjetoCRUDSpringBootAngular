2c2a469cadb9bcba4587273197dbae711a4dd99a
Jean <engjeanr@gmail.com>
2023-06-13 03:28:07 -0300

    AlunoRepository criado.

---
+++ src/main/java/github/jeanalvesr/domain/repositories/AlunoRepository.java
@@ -0,0 +1,9 @@
+package github.jeanalvesr.domain.repositories;
+
+import github.jeanalvesr.domain.entities.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+}
